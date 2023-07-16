1cd273ddd038b01bb3510a1caae8dd6168c92213
Daniel <pepelacon@gmail.com>
2023-07-16 09:09:08 -0400

    add lyaout

--- src/core/router/router.js
+++ src/core/router/router.js
@@ -1,19 +1,39 @@
+import { Layout } from '@/components/layout/layout.component'
 import { ROUTES } from './router.data'
 import { NotFound } from '@/components/screens/not-found/not-found.component'
 
 export class Router {
-	#routes
-	#currentRoute
+	#routes = ROUTES
+	#currentRoute = null
+	#layout = null
 
 	constructor() {
-		this.#routes = ROUTES
-		this.#currentRoute = null
-
+		window.addEventListener('popstate', () => {
+			this.#handleRouteChange()
+		})
 		this.#handleRouteChange()
+		this.#handleLinks()
+	}
+
+	#handleLinks() {
+		document.addEventListener('click', e => {
+			const target = e.target.closest('a')
+			if (target) {
+				e.preventDefault()
+				this.navigate(target.href)
+			}
+		})
 	}
 
 	getCurrentPath() {
 		return window.location.pathname
+	}
+
+	navigate(path) {
+		if (path !== this.getCurrentPath()) {
+			window.history.pushState({}, '', path)
+			this.#handleRouteChange()
+		}
 	}
 
 	#handleRouteChange() {
@@ -25,10 +45,20 @@ export class Router {
 			}
 		}
 		this.#currentRoute = route
-		this.render()
+		this.#render()
 	}
-	render() {
+	#render() {
 		const component = new this.#currentRoute.component()
-		document.getElementById('app').innerHTML = component.render()
+
+		if (!this.#layout) {
+			this.#layout = new Layout({
+				router: this,
+				children: component.render()
+			})
+
+			document.getElementById('app').innerHTML = this.#layout.render()
+		} else {
+			document.querySelector('main').innerHTML = component.render()
+		}
 	}
 }
