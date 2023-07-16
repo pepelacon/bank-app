7df6e08dd490a33deb1f9f2e8238e5829ffc2483
Daniel <pepelacon@gmail.com>
2023-07-16 12:50:24 -0400

    replace component names

--- src/core/services/render.service.js
+++ src/core/services/render.service.js
@@ -1,1 +1,57 @@
-class RenderService {}
+import { Component } from 'react'
+
+class RenderService {
+	/**
+	 * @param {string} html
+	 * @param {Array} components
+	 * @param {Object} [styles]
+	 * @returns {HTMLElement}
+	 */
+	htmlToElement(html, components = [], styles) {
+		const template = document.createElement('template')
+		template.innerHTML = html.trim()
+
+		const element = template.content.firstChild
+		this.#replaceComponentTags(element, components)
+
+		return element
+	}
+
+	/**
+	 * @param {HTMLElement} parentElement
+	 * @param {Array} components
+	 */
+	#replaceComponentTags(parentElement, components) {
+		const componentTagPattern = /^component-/
+		const allElements = parentElement.getElementsByTagName('*')
+
+		for (const element of allElements) {
+			const elementTagName = element.tagName.toLocaleLowerCase()
+			if (componentTagPattern.test(elementTagName)) {
+				const componentName = elementTagName
+					.replace(componentTagPattern, '')
+					.replace(/-/g, '')
+
+				const foundComponent = components.find(Component => {
+					const instance =
+						Component instanceof ChildComponent ? Component : new Component()
+					return instance.constructor.name.toLocaleLowerCase() === componentName
+				})
+
+				if (foundComponent) {
+					const componentContent =
+						foundComponent instanceof ChildComponent
+							? foundComponent.render()
+							: new foundComponent().render()
+					element.replaceWith(componentContent)
+				} else {
+					console.error(
+						`Component "${componentName}" not found in the provided components array.`
+					)
+				}
+			}
+		}
+	}
+}
+
+export default new RenderService()
