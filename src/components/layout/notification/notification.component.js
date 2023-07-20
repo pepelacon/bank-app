24cfc220b6216028aaf7e3c7ce9fe2392b15e7c5
Daniel <pepelacon@gmail.com>
2023-07-20 13:42:54 -0400

    add notifcation service and component

---
+++ src/components/layout/notification/notification.component.js
@@ -0,0 +1,13 @@
+import ChildComponent from '@/core/component/child.component'
+import renderService from '@/core/services/render.service'
+import { StorageService } from '../../../core/services/storage.service'
+
+import styles from './notification.module.scss'
+import template from './notification.template.html'
+
+export class Notification extends ChildComponent {
+	render() {
+		this.element = renderService.htmlToElement(template, [], styles)
+		return this.element
+	}
+}
